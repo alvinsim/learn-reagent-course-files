652791d430575bb44133e1a353c8ec0e5b5cf4ce
alvinsim <sim.alvin@gmail.com>
2018-08-07 11:55:21 +1000

    completed lessons 05 - 10

--- giggin/src/giggin/components/gigs.cljs
+++ giggin/src/giggin/components/gigs.cljs
@@ -1,4 +1,17 @@
-(ns giggin.components.gigs)
+(ns giggin.components.gigs
+  (:require [giggin.state :as state]))
 
 (defn gigs []
-  [:gigs "gigs"])
+  [:main
+   [:div.gigs
+    (map (fn [{:keys [id img title price desc]}]
+           [:div.gig {:key id}
+            [:img.gig__artwork {:src img :alt title}]
+            [:div.gig__body
+             [:div.gig__title
+              [:div.btn.btn--primary.float--right.tooltip
+               {:data-tooltip "Add to order"
+                :on-click (fn [] (swap! state/orders update id inc))}
+               [:i.icon.icon--plus]] title]
+             [:p.gig__price price]
+             [:p.gig__desc desc]]]) (vals @state/gigs))]])
